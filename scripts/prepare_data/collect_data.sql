4787874820a3aa2c343819a4d685af034a07ab21
michalsobczak <michal.sobczak@synerise.com>
2026-08-30 17:12:19 +0200

    fix(prepare-data): replace repo-star-limited clustering indicators

--- scripts/prepare_data/collect_data.sql
+++ scripts/prepare_data/collect_data.sql
@@ -1,13 +1,18 @@
+CREATE OR REPLACE VIEW repository_owners AS
+SELECT
+    url,
+    SPLIT_PART(full_name, '/', 1) AS owner
+FROM all_repository;
+
 SELECT
     au.login,
     COALESCE(au.followers, 0) AS followers,
     COALESCE(rep_stats.max_stars, 0) AS max_stars,
     COALESCE(rep_stats.max_forks, 0) AS max_forks,
     (2026 - CAST(SUBSTR(au.created_at, 1, 4) AS INT)) AS account_age,
-    COALESCE(hpr.human_pr_count, 0) AS human_prs,
-    COALESCE(comm_counts.total_commits, 0) AS total_commits,
-    COALESCE(rev_counts.total_reviews, 0) AS total_reviews,
-    COALESCE(iss_counts.total_issues, 0) AS total_issues
+    COALESCE(pr_stats.activity_span_days, 0) AS agent_activity_span_days,
+    COALESCE(pr_stats.agent_diversity, 0) AS agent_diversity,
+    COALESCE(pr_stats.repo_breadth, 0) AS agentic_repo_breadth
 FROM all_user au
 LEFT JOIN (
     SELECT
@@ -19,18 +24,11 @@ LEFT JOIN (
     GROUP BY ro.owner
 ) rep_stats ON au.login = rep_stats.owner
 LEFT JOIN (
-    SELECT user_id, COUNT(id) AS human_pr_count
-    FROM human_pull_request GROUP BY user_id
-) hpr ON au.id = hpr.user_id
-LEFT JOIN (
-    SELECT author, COUNT(pr_id) AS total_commits
-    FROM pr_commits GROUP BY author
-) comm_counts ON au.login = comm_counts.author
-LEFT JOIN (
-    SELECT user, COUNT(id) AS total_reviews
-    FROM pr_reviews WHERE user_type = 'User' GROUP BY user
-) rev_counts ON au.login = rev_counts.user
-LEFT JOIN (
-    SELECT user, COUNT(id) AS total_issues
-    FROM issue GROUP BY user
-) iss_counts ON au.login = iss_counts.user;
+    SELECT
+        user_id,
+        CAST(MAX(created_at) AS DATE) - CAST(MIN(created_at) AS DATE) AS activity_span_days,
+        COUNT(DISTINCT agent) AS agent_diversity,
+        COUNT(DISTINCT repo_id) AS repo_breadth
+    FROM all_pull_request
+    GROUP BY user_id
+) pr_stats ON au.id = pr_stats.user_id;
